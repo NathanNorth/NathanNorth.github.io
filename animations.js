6a0f9804853773a7589e204c349352774ec36c5d
NathanNorth <51377975+NathanNorth@users.noreply.github.com>
2021-05-16 21:40:55 -0400

    fade / link fixes

--- animations.js
+++ animations.js
@@ -1,9 +1,25 @@
 function loadingAnims() {
-    items = document.getElementsByClassName("aProject");
-    console.log(items.length);
-    for(let i = 0; i < items.length; i++) {
-        setTimeout(() => {animateIn(items.item(i));}, i * 250)
+    fades = document.getElementsByClassName("fadeable");
+    for(let i = 0; i < fades.length; i++) {
+        fadeIn(fades.item(i));
     }
+
+    cards = document.getElementsByClassName("aProject");
+    for(let i = 0; i < cards.length; i++) {
+        setTimeout(() => {animateIn(cards.item(i));}, i * 250)
+    }
+}
+function fadeIn(x) {
+    let id = setInterval(frame, 10);
+    let pos = 0;
+    function frame() {
+        pos += 1.5;
+        x.style.opacity = pos + '%';
+        if(pos > 100) {
+            clearInterval(id);
+        }
+    }
+
 }
 function animateIn(x) {
     let id = setInterval(frame, 10);
