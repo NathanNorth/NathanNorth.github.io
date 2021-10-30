0a9caca5d4782e7df83502c58636ec2d1db8ffb9
NathanNorth <51377975+NathanNorth@users.noreply.github.com>
2021-10-30 17:05:34 -0400

    mobile fixes?

--- animations.js
+++ animations.js
@@ -1,24 +1,27 @@
 function loadingAnims() {
     let fades = document.getElementsByClassName("fadeable");
-    for(let i = 0; i < fades.length; i++) {
+    for (let i = 0; i < fades.length; i++) {
         fadeIn(fades.item(i));
     }
 
     let cards = document.getElementsByClassName("aSlide");
-    for(let i = 0; i < cards.length; i++) {
+    for (let i = 0; i < cards.length; i++) {
         animateIn(cards.item(i), i);
     }
 
     let texts = document.getElementsByClassName("writable");
-    for(let i = 0; i < texts.length; i++) {
-        setTimeout(() => {writeIn(texts.item(i));}, i * 250)
+    for (let i = 0; i < texts.length; i++) {
+        setTimeout(() => {
+            writeIn(texts.item(i));
+        }, i * 250)
     }
 
     let lameTexts = document.getElementsByClassName("writableNR");
-    for(let i = 0; i < lameTexts.length; i++) {
+    for (let i = 0; i < lameTexts.length; i++) {
         writeInNoRando(lameTexts.item(i), i);
     }
 }
+
 function writeIn(x) {
     let element = x;
     let original = x.innerHTML;
@@ -27,8 +30,7 @@ function writeIn(x) {
         pos++;
         if (pos === 21) {
             clearInterval(id);
-        }
-        else {
+        } else {
             let chars = Math.round((original.length * pos) / 20);
             let arr = original.split(" "); //remove spaces
             arr = arr.reduce((a, b) => a + b, "").split(""); //reduce to string then re-split
@@ -46,19 +48,22 @@ function writeIn(x) {
         }
     }, 60);
 }
+
 function writeInNoRando(x, y) {
     let element = x;
     let original = x.innerHTML;
     element.innerHTML = "&nbsp";
-    setTimeout(() => {doTheThing();}, y * 250);
+    setTimeout(() => {
+        doTheThing();
+    }, y * 250);
+
     function doTheThing() {
         let pos = 0;
         let id = setInterval(async () => {
             pos++;
             if (pos === 21) {
                 clearInterval(id);
-            }
-            else {
+            } else {
                 let chars = Math.round((original.length * pos) / 20);
                 let arr = original.split(" "); //remove spaces
                 arr = arr.reduce((a, b) => a + b, "").split(""); //reduce to string then re-split
@@ -77,33 +82,39 @@ function writeInNoRando(x, y) {
         }, 30);
     }
 }
+
 function fadeIn(x) {
     let id = setInterval(frame, 10);
     let pos = 0;
+
     function frame() {
         pos += 1.5;
         x.style.opacity = pos + '%';
-        if(pos > 100) {
+        if (pos > 100) {
             clearInterval(id);
         }
     }
 
 }
+
 function animateIn(x, y) {
     x.style.visibility = 'hidden';
-    setTimeout(() => {doTheThing(x);}, y * 250);
+    setTimeout(() => {
+        doTheThing(x);
+    }, y * 250);
+
     function doTheThing(x) {
         fadeIn(x);
         let id = setInterval(frame, 10);
         let pos = 1000;
+
         function frame() {
-            if(Math.abs(pos) < 1) {
+            if (Math.abs(pos) < 1) {
                 x.style.left = '0px';
                 clearInterval(id);
-            }
-            else {
+            } else {
                 pos -= (pos / 15);
-                x.style.left = pos +'px';
+                x.style.left = pos + 'px';
                 x.style.visibility = 'visible';
             }
         }
